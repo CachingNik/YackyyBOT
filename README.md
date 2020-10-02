0932ffd2c6d4ad2f21f045590f873a38143bc92e
Nikhil Gupta <nikhil.gupta6121999@gmail.com>
2020-10-02 14:19:31 +0530

    Added shutdown & reset command for admin

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Chat with Yackyy (AI DISCORD BOT)
+Yackyy is a discord bot made using python and AIML. AIML is Artificial 
+Intelligence Markup Language.\
+You can know more about it from here: http://www.aiml.foundation/doc.html
+
+This bot only works in a channel named as `chat-with-yackyy`.
+
+There is also a user cooldown feature which gets triggered after 
+a user types more than 10 messages inside 30 mins. The\
+*PS: Each User has its own cooldown for 5 mins.*
+
+Screenshots:\
+![image1](Screenshots/s1.png "1")
--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import os
 import datetime
 
 
-client = commands.Bot(command_prefix="!")
+client = commands.Bot(command_prefix='!')
 
 
 cluster = MongoClient(os.getenv("DB"))
@@ -19,6 +19,7 @@ k = aiml.Kernel()
 
 @client.event
 async def on_ready():
+    channel = client.get_channel(761233422567604254)
     k.setBotPredicate("name", "Yackyy")
     if os.path.exists(BRAIN_FILE):
         print("Loading from brain file...")
@@ -28,6 +29,7 @@ async def on_ready():
         k.bootstrap(learnFiles="std-startup.xml", commands="load aiml b")
         print("Saving brain file...")
         k.saveBrain(BRAIN_FILE)
+    await channel.send("🟢 ONLINE")
     print("Bot is Ready")
 
 
@@ -37,22 +39,19 @@ async def on_message(msg):
     if msg.channel.name != "chat-with-yackyy":
         return
 
-    if msg.content == "/res":
-        return
-
     if msg.author.bot:
         return
 
-    my_query = {"User": msg.author.name}
+    my_query = {"User": msg.author.id}
 
-    if msg.author.name != "BratherNik":
+    if msg.author.id != 390755289038848000:
         if cdu.count_documents(my_query) > 0:
             for user in cdu.find(my_query):
                 duration = datetime.datetime.now() - user['Time']
                 if user['Score'] == 10 and duration.total_seconds() < 1800:
                     print("Time left to remove cool down:", 300 - duration.total_seconds())
                     if duration.total_seconds() < 300:
-                        await msg.channel.send(str(msg.author.mention) + ", You are on cool down for " + "{:.2f}".format(300-duration.total_seconds()) + "secs")
+                        await msg.channel.send(str(msg.author.mention) + ", You are on cool down for " + "{:.2f}".format(300-duration.total_seconds()) + " secs")
                         print("Cool Down Mode!")
                         return
                     else:
@@ -62,7 +61,7 @@ async def on_message(msg):
                     cdu.update_one(my_query, {"$inc": {"Score": 1}, "$set": {"Time": datetime.datetime.now()}})
                     print("Updating cds for a user...")
         else:
-            post = {"User": msg.author.name, "Score": 1, "Time": datetime.datetime.now()}
+            post = {"User": msg.author.id, "Score": 1, "Time": datetime.datetime.now()}
             cdu.insert_one(post)
             print("Inserting new user cds...")
 
@@ -70,12 +69,41 @@ async def on_message(msg):
     for ch in ['/', "'", ".", "\\", "(", ")", '"', '\n', '@', '<', '>']:
         text = text.replace(ch, '')
 
-    response = k.respond(text)
-    response = response.replace("://", "")
-    response = response.replace("@", "")
-    response = "`@%s`: %s" % (msg.author.name, response)
+    if text != "!shutdown" and text != "!reset" and text != "!sc":
+        response = k.respond(text)
+        response = response.replace("://", "")
+        response = response.replace("@", "")
+        response = "`@%s`: %s" % (msg.author.name, response)
+        await msg.channel.send(response)
 
-    await msg.channel.send(response)
+    await client.process_commands(msg)
+
+
+@client.command()
+@commands.has_permissions(administrator=True)
+async def shutdown(ctx):
+    print("Shutting Down...")
+    await ctx.send("🔴 OFFLINE")
+    await ctx.bot.logout()
+
+
+@client.command()
+@commands.has_permissions(administrator=True)
+async def reset(ctx):
+    await ctx.send("Resetting my brain...")
+    print("Brain Reset")
+    k.resetBrain()
+    k.setBotPredicate("name", "Yackyy")
+    k.learn("std-startup.xml")
+    k.respond("load aiml b")
+    k.saveBrain(BRAIN_FILE)
+    await ctx.send("✅ Done")
+
+
+@client.command()
+@commands.has_permissions(administrator=True)
+async def sc(ctx):
+    await ctx.send("https://github.com/CachingNik/YackyyBOT")
 
 
 client.run(os.getenv("TOKEN"))
